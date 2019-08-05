6a2e0c5099101c02fd9ce54063e1f1f317a03c4d
jsdevtom <contact@jsdevtom.com>
2019-08-05 18:25:45 +0200

    docs: write about the acctual module in the README, correct jsdoc

--- src/distinct-until-changed-immutable.ts
+++ src/distinct-until-changed-immutable.ts
@@ -1,12 +1,12 @@
 /**
- * Returns an Observable that emits all items emitted by the source Observable that are distinct by comparison from the previous item.
+ * Returns an Observable that emits all items emitted by the source Observable that are deeply distinct by comparison from the previous item.
  *
  * If a comparator function is provided, then it will be called for each item to test for whether or not that value should be emitted.
  *
- * If a comparator function is not provided, an equality check is used by default.
+ * If a comparator function is not provided, a deep equality check is used by default.
  *
  * ## Example
- * A simple example with numbers
+ * A simple example with objects
  * ```javascript
  * import { of } from 'rxjs';
  * import { distinctUntilChangedImmutable } from 'distinct-until-changed-immutable';
@@ -33,7 +33,7 @@
  *     { age: 4, name: 'Foo'},
  *     { age: 6, name: 'Foo'},
  *   ).pipe(
- *     distinctUntilChanged((p: Person, q: Person) => p === q),
+ *     distinctUntilChangedImmutable((p: Person /*I have been cloned*\/, q: Person) => p === q),
  *   )
  *   .subscribe(x => console.log(x));
  *
